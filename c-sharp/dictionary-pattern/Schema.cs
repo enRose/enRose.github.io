0253eb191de932ecef409bf5e3d4281c2af803ec
enRose <34562396+enRose@users.noreply.github.com>
2020-04-20 11:12:02 +1200

    Create Schema.cs

---
+++ c-sharp/dictionary-pattern/Schema.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Barin.API.DictionaryPattern
+{
+    public class Data
+    {
+        public string Eligibility { get; set; }
+        public SpiderMan SpiderMan { get; set; }
+        public ThanosDto ThanosDto { get; set; }
+        public IronMan IronMan { get; set; }
+        public CaptainAmerica CaptainAmerica { get; set; }
+        public WandaMaximoff WandaMaximoff { get; set; }
+        public Thor Thor { get; set; }
+    }
+}
