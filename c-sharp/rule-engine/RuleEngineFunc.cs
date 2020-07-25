6fdb305646b0e5bf099c10b03615cf12c5ceabdb
enRose <34562396+enRose@users.noreply.github.com>
2020-07-26 01:37:41 +1200

    Update RuleEngineFunc.cs

--- c-sharp/rule-engine/RuleEngineFunc.cs
+++ c-sharp/rule-engine/RuleEngineFunc.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace API.Barin.RuleEngine.
+namespace API.Barin.RuleEngine
 {
     public class RuleResult
     { 
